f7c5fc5426155a568907482434cd33dc0dc29815
inphi <mlaw2501@gmail.com>
2022-04-25 13:20:18 -0400

    cache debug_traceTransaction Calls
    Rosetta clients can no longer short-circuit debug_traceTransaction RPCs
    induced by /block requests. These RPCs will continue executing in the
    background and their result cached for faster retrieval in the future.
    
    This feature is only enabled when the `ENABLE_TRACE_CACHE` envar is set to
    true.

---
+++ optimism/tracer_test.go
@@ -0,0 +1,88 @@
+package optimism
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	mocks "github.com/coinbase/rosetta-ethereum/mocks/optimism"
+	"github.com/ethereum-optimism/optimism/l2geth/common"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestTracerFetch_HappyPath(t *testing.T) {
+	ctx := context.Background()
+
+	mockClient := &mocks.JSONRPC{}
+	cache, err := NewTraceCache(mockClient, "call_tracer.js", time.Second*1, 10)
+	assert.NoError(t, err)
+
+	tc, err := loadTraceConfig("call_tracer.js", time.Second*1)
+	assert.NoError(t, err)
+	hash := common.HexToHash("0x5e77a04531c7c107af1882d76cbff9486d0a9aa53701c30888509d4f5f2b003a")
+
+	expect := Call{Type: "ekans"}
+	mockClient.On(
+		"CallContext",
+		mock.Anything,
+		mock.Anything,
+		"debug_traceTransaction",
+		mock.Anything,
+	).Return(
+		nil,
+	).Run(
+		func(args mock.Arguments) {
+			arg := args.Get(3).([]interface{})
+			assert.Equal(
+				t,
+				common.HexToHash("0x5e77a04531c7c107af1882d76cbff9486d0a9aa53701c30888509d4f5f2b003a").Hex(),
+				arg[0],
+			)
+			assert.Equal(t, tc, arg[1])
+
+			r := args.Get(1).(*Call)
+
+			*r = expect
+		},
+	).Once()
+
+	call, err := cache.FetchTransaction(ctx, hash)
+	assert.NoError(t, err)
+	assert.Equal(t, expect, *call)
+}
+
+func TestTracerFetch_ExpiredContext(t *testing.T) {
+	ctx := context.Background()
+
+	mockClient := &mocks.JSONRPC{}
+	cache, err := NewTraceCache(mockClient, "call_tracer.js", time.Second*1, 10)
+	assert.NoError(t, err)
+
+	expect := Call{Type: "ekans"}
+	mockClient.On(
+		"CallContext",
+		mock.Anything,
+		mock.Anything,
+		"debug_traceTransaction",
+		mock.Anything,
+	).Return(
+		nil,
+	).Run(
+		func(args mock.Arguments) {
+			time.Sleep(time.Millisecond * 500)
+			r := args.Get(1).(*Call)
+			*r = expect
+		},
+	).Once()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*1)
+	_, err = cache.FetchTransaction(ctx, common.Hash{})
+	cancel()
+	assert.EqualError(t, err, context.DeadlineExceeded.Error())
+
+	// The JSONRPC is mocked only once so the second call reads existing value from the cache
+	call, err := cache.FetchTransaction(context.Background(), common.Hash{})
+	assert.NoError(t, err)
+	assert.Equal(t, expect, *call)
+}
