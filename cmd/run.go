f26d01855552a0806c9839514f9942b5d71eeda9
Qi <wqi@umich.edu>
2022-09-17 01:23:51 +0800

    Use lowercase

--- cmd/run.go
+++ cmd/run.go
@@ -151,7 +151,7 @@ func getSupportedTokens(network string) map[string]bool {
 			"0x4200000000000000000000000000000000000042": true, // OP
 			"0xda10009cbd5d07dd0cecc66161fc93d7c9000da1": true, // DAI
 			"0x2e5ed97596a8368eb9e44b1f3f25b2e813845303": true, // SNX
-			"0x853eb4bA5D0Ba2B77a0A5329Fd2110d5CE149ECE": true, // USDT
+			"0x853eb4ba5d0ba2b77a0a5329fd2110d5ce149ece": true, // USDT
 			"0xe0a592353e81a94db6e3226fd4a99f881751776a": true, // WBTC
 			// "": true, // USDC
 		}
