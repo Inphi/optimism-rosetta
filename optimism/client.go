4eed7446df068aad90472d55415b7935a105017d
Andreas Bigger <abigger87@gmail.com>
2023-02-03 17:06:25 -0500

    fix: test estimate gas refactor

--- optimism/client.go
+++ optimism/client.go
@@ -942,33 +942,6 @@ func (ec *Client) estimateGas(
 	}, nil
 }
 
-// EstimateGas retrieves the currently gas limit
-func (ec *Client) EstimateGas(ctx context.Context, msg ethereum.CallMsg) (uint64, error) {
-	arg := map[string]interface{}{
-		"from": msg.From,
-		"to":   msg.To,
-	}
-	if len(msg.Data) > 0 {
-		arg["data"] = hexutil.Bytes(msg.Data)
-	}
-	if msg.Value != nil {
-		arg["value"] = (*hexutil.Big)(msg.Value)
-	}
-	if msg.Gas != 0 {
-		arg["gas"] = hexutil.Uint64(msg.Gas)
-	}
-	if msg.GasPrice != nil {
-		arg["gasPrice"] = (*hexutil.Big)(msg.GasPrice)
-	}
-
-	var hex hexutil.Uint64
-	err := ec.c.CallContext(ctx, &hex, "eth_estimateGas", arg)
-	if err != nil {
-		return 0, err
-	}
-	return uint64(hex), nil
-}
-
 func validateCallInput(params map[string]interface{}) (*GetCallInput, error) {
 	var input GetCallInput
 	if err := RosettaTypes.UnmarshalMap(params, &input); err != nil {
---
+++ optimism/client_estimate_gas_test.go
@@ -0,0 +1,90 @@
+package optimism
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"testing"
+
+	ethereum "github.com/ethereum-optimism/optimism/l2geth"
+	"github.com/ethereum-optimism/optimism/l2geth/common"
+	"github.com/ethereum-optimism/optimism/l2geth/common/hexutil"
+	mocks "github.com/inphi/optimism-rosetta/mocks/optimism"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"golang.org/x/sync/semaphore"
+)
+
+// ClientEstimateGasSuite tests [Client.EstimateGas].
+type ClientEstimateGasSuite struct {
+	suite.Suite
+
+	mockJSONRPC *mocks.JSONRPC
+	mockGraphQL *mocks.GraphQL
+	client      *Client
+}
+
+// SetupTest sets up the test suite.
+func (testSuite *ClientEstimateGasSuite) SetupTest() {
+	testSuite.mockJSONRPC = &mocks.JSONRPC{}
+	testSuite.mockGraphQL = &mocks.GraphQL{}
+	cf, err := newERC20CurrencyFetcher(testSuite.mockJSONRPC)
+	testSuite.NoError(err)
+	testSuite.client = &Client{
+		c:               testSuite.mockJSONRPC,
+		g:               testSuite.mockGraphQL,
+		currencyFetcher: cf,
+		traceSemaphore:  semaphore.NewWeighted(100),
+	}
+}
+
+// TestSuite runs the ClientEstimateGasSuite.
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(ClientEstimateGasSuite))
+}
+
+// TestClientEstimateGas tests [Client.EstimateGas].
+func (testSuite *ClientEstimateGasSuite) TestClientEstimateGas() {
+	ctx := context.Background()
+	from := common.HexToAddress("0xE550f300E477C60CE7e7172d12e5a27e9379D2e3")
+	to := common.HexToAddress("0xaD6D458402F60fD3Bd25163575031ACDce07538D")
+	data := common.FromHex("0xa9059cbb000000000000000000000000ae7e48ee0f758cd706b76cf7e2175d982800879a" +
+		"00000000000000000000000000000000000000000000000000521c5f98b8ea00")
+	testSuite.mockJSONRPC.On(
+		"CallContext",
+		ctx,
+		mock.Anything,
+		"eth_estimateGas",
+		mock.Anything,
+	).Return(
+		nil,
+	).Run(
+		func(args mock.Arguments) {
+			r := args.Get(1).(*hexutil.Uint64)
+
+			var expected map[string]interface{}
+			file, err := os.ReadFile("testdata/estimate_gas_0xaD6D458402F60fD3Bd25163575031ACDce07538D.json")
+			testSuite.NoError(err)
+
+			err = json.Unmarshal(file, &expected)
+			testSuite.NoError(err)
+
+			*r = hexutil.Uint64(1)
+		},
+	).Once()
+
+	correctRaw, err := os.ReadFile("testdata/estimate_gas_0xaD6D458402F60fD3Bd25163575031ACDce07538D.json")
+	testSuite.NoError(err)
+	var correct map[string]interface{}
+	testSuite.NoError(json.Unmarshal(correctRaw, &correct))
+
+	resp, err := testSuite.client.EstimateGas(ctx, ethereum.CallMsg{
+		From: from,
+		To:   &to,
+		Data: data,
+	})
+
+	testSuite.Equal(uint64(1), resp)
+	testSuite.NoError(err)
+}
