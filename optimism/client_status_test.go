86bfed2d598981e292d8ff26be08aead7da2ccbb
Inphi <mlaw2501@gmail.com>
2024-03-14 13:38:51 -0400

    Add /network/status tests for ecotone (#109)

--- optimism/client_status_test.go
+++ optimism/client_status_test.go
@@ -25,7 +25,6 @@ import (
 
 	RosettaTypes "github.com/coinbase/rosetta-sdk-go/types"
 	ethereum "github.com/ethereum-optimism/optimism/l2geth"
-	"github.com/ethereum-optimism/optimism/l2geth/core/types"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -92,11 +91,11 @@ func TestStatus_NotSyncing(t *testing.T) {
 		nil,
 	).Run(
 		func(args mock.Arguments) {
-			header := args.Get(1).(**types.Header)
+			header := args.Get(1).(**rpcHeader)
 			file, err := os.ReadFile("testdata/basic_header.json")
 			assert.NoError(t, err)
 
-			*header = new(types.Header)
+			*header = new(rpcHeader)
 
 			assert.NoError(t, (*header).UnmarshalJSON(file))
 		},
@@ -165,11 +164,11 @@ func TestStatus_Syncing(t *testing.T) {
 		nil,
 	).Run(
 		func(args mock.Arguments) {
-			header := args.Get(1).(**types.Header)
+			header := args.Get(1).(**rpcHeader)
 			file, err := os.ReadFile("testdata/basic_header.json")
 			assert.NoError(t, err)
 
-			*header = new(types.Header)
+			*header = new(rpcHeader)
 
 			assert.NoError(t, (*header).UnmarshalJSON(file))
 		},
@@ -204,6 +203,79 @@ func TestStatus_Syncing(t *testing.T) {
 	assert.Equal(t, &RosettaTypes.SyncStatus{
 		CurrentIndex: RosettaTypes.Int64(8916656),
 		TargetIndex:  RosettaTypes.Int64(8916656),
+	}, syncStatus)
+	assert.Equal(t, peers, []*RosettaTypes.Peer{})
+	assert.NoError(t, err)
+
+	mockJSONRPC.AssertExpectations(t)
+	mockGraphQL.AssertExpectations(t)
+}
+
+func TestStatus_Ecotone_Syncing(t *testing.T) {
+	mockJSONRPC := &mocks.JSONRPC{}
+	mockGraphQL := &mocks.GraphQL{}
+	cf, err := newERC20CurrencyFetcher(mockJSONRPC)
+	assert.NoError(t, err)
+
+	c := &Client{
+		c:               mockJSONRPC,
+		g:               mockGraphQL,
+		currencyFetcher: cf,
+		traceSemaphore:  semaphore.NewWeighted(100),
+		supportsSyncing: true,
+	}
+
+	ctx := context.Background()
+	mockJSONRPC.On(
+		"CallContext",
+		ctx,
+		mock.Anything,
+		"eth_getBlockByNumber",
+		"safe",
+		false,
+	).Return(
+		nil,
+	).Run(
+		func(args mock.Arguments) {
+			header := args.Get(1).(**rpcHeader)
+			file, err := os.ReadFile("testdata/ecotone_header.json")
+			assert.NoError(t, err)
+
+			*header = new(rpcHeader)
+
+			assert.NoError(t, (*header).UnmarshalJSON(file))
+		},
+	).Once()
+
+	mockJSONRPC.On(
+		"CallContext",
+		ctx,
+		mock.Anything,
+		"eth_syncing",
+	).Return(
+		nil,
+	).Run(
+		func(args mock.Arguments) {
+			rpcp := args.Get(1).(*json.RawMessage)
+			j, err := json.Marshal(&rpcProgress{
+				CurrentBlock: hexutil.Uint64(117418562),
+				HighestBlock: hexutil.Uint64(117418562),
+			})
+			assert.NoError(t, err)
+			rawMessage := json.RawMessage(j)
+			*rpcp = rawMessage
+		},
+	).Once()
+
+	block, timestamp, syncStatus, peers, err := c.Status(ctx)
+	assert.Equal(t, &RosettaTypes.BlockIdentifier{
+		Hash:  "0x0be3b6bfa35290d847039ae03d72e6d54d9fd053232e8be4facf1aa0d1040588",
+		Index: 117418562,
+	}, block)
+	assert.Equal(t, int64(1710435901000), timestamp)
+	assert.Equal(t, &RosettaTypes.SyncStatus{
+		CurrentIndex: RosettaTypes.Int64(117418562),
+		TargetIndex:  RosettaTypes.Int64(117418562),
 	}, syncStatus)
 	assert.Equal(t, peers, []*RosettaTypes.Peer{})
 	assert.NoError(t, err)
